a00e84d023324043103c8b253c4306873963a6d9
Eynar92 <eynar.j.alvarez.m@gmail.com>
2024-10-28 11:37:19 -0400

    feat users.py with users methods

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from app.routes.main import main
+from app.routes.users import user_routes
+
 
 def create_app():
     app = Flask(__name__)
-    from app.routes import main
+
+    # Blueprint register
     app.register_blueprint(main)
+    app.register_blueprint(user_routes)
+
+    # Aditional config
+    app.config['UPLOAD_FOLDER'] = 'app/uploads'
+    
     return app
